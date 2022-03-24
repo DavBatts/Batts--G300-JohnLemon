bb99a42d1b9079153ce9fd1ad9a4affa30227e6b
DavBatts <dsbatts13@gmail.com>
2022-03-24 12:09:37 -0400

    Update Advanced Ghost
    A good stopping point before laptop dies

--- John-Lemon/Assets/Scripts/AdvancedGhost.cs
+++ John-Lemon/Assets/Scripts/AdvancedGhost.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AdvancedGhost : MonoBehaviour
+public class advancedghost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
+public Transform player;
+
+public LayerMask whatIsGround, whatIsPlayer;
+
+//Patrolling
+public Vector3 walkPoint;
+bool walkPointSet;
+public float walkPointRange;
+
+//Attacking
+public float timeBetweenAttacks;
+bool alreadyAttacked;
+
+//States
+public float sightRange, attackRange;
+public bool playerInSightRange, playerInAttackRange;
+
+private void awake()
+{
+    player = GameObject.Find("JohnLemon").transform;
+    agent = GetComponent<NavMeshAgent>();
+}
+
+private void Update()
+{
+    //Check for player in sight and attack range
+    playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+    playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+    if (!playerInSightRange && !playerInAttackRange) Patrolling();
+    if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+    if (playerInAttackRange && playerInSightRange) AttackPlayer();
     {
         
     }
+}
+
+private void Patrolling()
+{
+
+}
+
+private void ChasePlayer()
+{
+
+}
+
+private void AttackPlayer()
+{
+
 }
